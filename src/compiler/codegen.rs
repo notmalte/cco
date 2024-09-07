3bcec2269f9fa7e4122f309c7ec0851563280e19
malte <malte@flex.earth>
2024-09-07 23:15:33 +0200

    Codegen tests

--- src/compiler/codegen.rs
+++ src/compiler/codegen.rs
@@ -1,23 +1,25 @@
-use super::parser::{Expression, Program as AstProgram, Statement};
+use super::parser::{
+    Expression as AstExpression, Program as AstProgram, Statement as AstStatement,
+};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Program {
     function_definition: Function,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Function {
     name: String,
     instructions: Vec<Instruction>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Instruction {
     Mov { src: Operand, dst: Operand },
     Ret,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Operand {
     Imm(i32),
     Register,
@@ -28,8 +30,8 @@ pub fn generate(ast_program: AstProgram) -> Program {
         function_definition: Function {
             name: ast_program.function_definition.name,
             instructions: match ast_program.function_definition.body {
-                Statement::Return(expr) => match expr {
-                    Expression::IntLiteral(value) => vec![
+                AstStatement::Return(expr) => match expr {
+                    AstExpression::IntLiteral(value) => vec![
                         Instruction::Mov {
                             src: Operand::Imm(value),
                             dst: Operand::Register,
@@ -40,4 +42,37 @@ pub fn generate(ast_program: AstProgram) -> Program {
             },
         },
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{super::parser::Function as AstFunction, *};
+
+    #[test]
+    fn test_generate() {
+        let ast_program = AstProgram {
+            function_definition: AstFunction {
+                name: "main".to_string(),
+                body: AstStatement::Return(AstExpression::IntLiteral(42)),
+            },
+        };
+
+        let program = generate(ast_program);
+
+        assert_eq!(
+            program,
+            Program {
+                function_definition: Function {
+                    name: "main".to_string(),
+                    instructions: vec![
+                        Instruction::Mov {
+                            src: Operand::Imm(42),
+                            dst: Operand::Register,
+                        },
+                        Instruction::Ret,
+                    ],
+                },
+            }
+        );
+    }
 }
