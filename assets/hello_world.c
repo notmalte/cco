9e0b537991169d47f76678070d512f96b169ddeb
malte <malte@flex.earth>
2024-10-04 00:50:04 +0200

    Add "Hello, World!" example

---
+++ assets/hello_world.c
@@ -0,0 +1,26 @@
+int putchar(int c);
+
+int hello_world(void) {
+    putchar(72);
+    putchar(101);
+    putchar(108);
+    putchar(108);
+    putchar(111);
+    putchar(44);
+    putchar(32);
+    putchar(87);
+    putchar(111);
+    putchar(114);
+    putchar(108);
+    putchar(100);
+    putchar(33);
+    putchar(10);
+
+    return 0;
+}
+
+int main(void) {
+    hello_world();
+
+    return 0;
+}
